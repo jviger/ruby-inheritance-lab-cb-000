7ab380725cce47452a18072cad249a5927640c8e
Jason Viger <jason.viger@gmail.com>
2018-08-21 07:40:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,11 @@
 require_relative "./user.rb"
 class Student < User
 
-  def initialize(knowledge)
+  def initialize
     @knowledge = []
+  end
+  
+  def learn(learned)
+    self.knowledge << learned
   end
 end
